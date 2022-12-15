4e2f4d1e20164b5053ef3ed6836a4a81c9cc004f
sachethpraveen <sacheth.praveen@card91.io>
2022-12-15 15:27:46 +0530

    modify implementation of cards page

--- pages/cards.tsx
+++ pages/cards.tsx
@@ -1,51 +1,61 @@
 import {
-  CardGroupTwo,
-  Header,
-  Pagination,
-  Sidebar,
-  Table,
-} from "@sachethpraveen/components";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useGetBinsQuery } from "../features/api/binsSlice";
-import { nanoid } from "@reduxjs/toolkit";
+    CardGroupTwo,
+    Header,
+    NewFilter,
+    NewTable,
+    Pagination,
+    Sidebar,
+    Table,
+} from '@sachethpraveen/components'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useGetBinsQuery } from '../features/api/apiBinsSlice'
+import { nanoid } from '@reduxjs/toolkit'
 
 const cards = () => {
-  const { data } = useGetBinsQuery();
+    const { data, isSuccess } = useGetBinsQuery()
 
-  return (
-    <div>
-      <Header />
-      <div className="d-flex">
+    return (
         <div>
-          <Sidebar />
-        </div>
-        <div className="ms-3 me-3 w-100">
-          <div className="fw-semibold fs-3">Cards</div>
-          <div>
-            <CardGroupTwo />
-          </div>
-          <Pagination />
-          {data ? (
-            <Table
-              data={data.map(
-                (product: { rating: { rate: number; count: number } }) => {
-                  return Object.fromEntries(
-                    Object.entries(product).slice(0, 5)
-                  );
-                }
-              )}
-              generateKey={nanoid}
-            />
-          ) : (
-            <div className="d-flex justify-content-center">
-              <div className="spinner-border"></div>
+            <div className="header">
+                <Header />
             </div>
-          )}
+            <div className="d-flex">
+                <div>
+                    <Sidebar />
+                </div>
+                <div className="ms-3 me-3 w-100">
+                    <div className="fw-semibold fs-3">Cards</div>
+                    <div>
+                        <CardGroupTwo />
+                    </div>
+                    <Pagination />
+                    {isSuccess ? (
+                        <NewTable
+                            receivedData={data.map((product: Object) => {
+                                return Object.fromEntries(
+                                    Object.entries(product).slice(0, 5)
+                                )
+                            })}
+                            receivedColumns={Object.keys(data[0])
+                                .slice(0, 5)
+                                .map((key) => {
+                                    return {
+                                        Header: key.toUpperCase(),
+                                        accessor: key,
+                                        Filter: NewFilter,
+                                    }
+                                })}
+                        />
+                    ) : (
+                        <div className="d-flex justify-content-center">
+                            <div className="spinner-border"></div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default cards;
+export default cards
